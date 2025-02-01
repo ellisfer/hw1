518efaa7681123b3bd93a9d4b32adf36bed4cb51
Ellis Fertig <efertig@usc.edu>
2025-02-01 04:18:41 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,27 @@
 
 int main(int argc, char* argv[])
 {
+  cout << "in main! " << endl;
 
+  ULListStr ullist;
+  ullist.push_back("sup");
+  ullist.push_back("er");
+  ullist.push_back("cal");
+  ullist.push_back("i");
+  ullist.push_back("frag");
+  ullist.push_back("il");
+  ullist.push_back("ist");
+  ullist.push_back("ic");
+  ullist.push_back("exp");
+  ullist.push_back("i");
+  ullist.push_back("alo");
+  ullist.push_back("doc");
+  ullist.push_back("ious");
+
+
+
+  cout << "front " << ullist.front() << " back " << ullist.back() << endl;
+
+
+  cout << "done!" << endl;
 }
